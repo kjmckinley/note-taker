ccbe8680f161303664d5e366d23c95b00ba4a8e7
Kyle <kj.mckinley@gmail.com>
2021-06-29 20:19:51 -0500

    added an app.delete function to api-routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,32 +1,51 @@
+
+
 const fs = require('fs');
+
 var noteData = JSON.parse(fs.readFileSync("./develop/db/db.json", "utf8"));
 
 
 module.exports = function (app) {
 
     app.get('/api/notes', function(req, res) {
-        res.json(data);
+        res.json(noteData);
     });
 
     app.get("/api/notes/:id", function(req, res) {
-        res.json(data[Number(req.params.id)]);
+        res.json(noteData[Number(req.params.id)]);
     });
 
     app.post("/api/notes", function(req, res) {
 
-        let uniqueId = (data.length).toString();
-        let newNote = req.body;
+        let uniqueId = (noteData.length).toString();
+        let newUserNote = req.body;
         console.log(uniqueId);
-        newNote.id = uniqueId;
-        data.push(newNote);
+        newUserNote.id = uniqueId;
+        noteData.push(newUserNote);
         
-        fs.writeFileSync("./develop/db/db.json", JSON.stringify(data), function(err) {
+        fs.writeFileSync("./develop/db/db.json", JSON.stringify(noteData), function(err) {
             if (err) throw (err);        
         }); 
     
-        res.json(data);    
+        res.json(noteData);    
     
     });
+
+    app.delete("/api/notes/:id", function(req, res) {
+
+        let newId = 0;
+        let idNote = req.params.id;
+        console.log(`Deleting note with id ${idNote}`);
+        noteData = noteData.filter(currentNote => {
+           return currentNote.id != idNote;
+        });
+        for (currentNote of noteData) {
+            currentNote.id = newId.toString();
+            newId++;
+        }
+        fs.writeFileSync("./develop/db/db.json", JSON.stringify(noteData));
+        res.json(noteData);
+    }); 
 
 }
 
