3290e708d40945c2d5923f400db42e3fdf65c605
Kyle <kj.mckinley@gmail.com>
2021-06-29 20:06:49 -0500

    built on api-routes.js

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+var noteData = JSON.parse(fs.readFileSync("./develop/db/db.json", "utf8"));
 
 
 module.exports = function (app) {
@@ -10,4 +11,22 @@ module.exports = function (app) {
     app.get("/api/notes/:id", function(req, res) {
         res.json(data[Number(req.params.id)]);
     });
+
+    app.post("/api/notes", function(req, res) {
+
+        let newNote = req.body;
+        let uniqueId = (data.length).toString();
+        console.log(uniqueId);
+        newNote.id = uniqueId;
+        data.push(newNote);
+        
+        fs.writeFileSync("./db/db.json", JSON.stringify(data), function(err) {
+            if (err) throw (err);        
+        }); 
+    
+        res.json(data);    
+    
+    });
+
 }
+
