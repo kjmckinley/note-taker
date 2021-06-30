3f4ce9cd6e149b80ee8dd9c975ecb738767947c3
Kyle <kj.mckinley@gmail.com>
2021-06-29 19:54:40 -0500

    built the server.js out

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 
+// constant variables that denote dependencies
 const express = require('express');
+const fs = require("fs");
 
+//sets up express
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
+// allows express to parse through data
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use("/Assets", express.static("./Assets"));
 
+require("./routes/html-routes")(app);
+require("./routes/api-routes")(app);
+
+// starts the server up!
 app.listen(PORT, function() {
     console.log(`Server is listening on PORT: ${PORT}`);
   });
